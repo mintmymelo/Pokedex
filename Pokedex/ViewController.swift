a5e4329f5a3afc260a3ba15478b835cdc00ce23a
Penpitcha Natisupalak <penpitchan@Penpitchas-MacBook-Pro.local>
2017-01-30 14:02:41 +0700

    first commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Penpitcha Natisupalak on 1/30/2560 BE.
+//  Copyright © 2560 Penpitcha Natisupalak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
